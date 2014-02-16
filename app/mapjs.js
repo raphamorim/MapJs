09478f87c0ffbdbe9c9ae2967daf0016c1d72596
Raphael Amorim <rapha850@gmail.com>
2014-02-16 01:19:32 -0300

    improve app js

--- app/mapjs.js
+++ app/mapjs.js
@@ -23,11 +23,11 @@ var mapProperties = {
       typeId: null
     };
 
-window.onload = __getElements;
+window.onload = __getElements();
+
 var __creating = window.setInterval(__getElements, 1000);
 
-
-function __getElements () {
+function __getElements() {
 
   var indice = helper.getQuantum(mapProperties.tag);
   var elemento = document.querySelectorAll(mapProperties.tag);
@@ -39,19 +39,34 @@ function __getElements () {
 
   clearInterval(__creating);
 
+  __addCss();
+
   __creating = window.setInterval(__startApp, 1000);
 
 }
 
+function __addCss() {
+  var newElement = document.createElement('div');
+  newElement.id = "mapcss";
+  document.querySelector('body').appendChild(newElement);
 
-function __startApp () {
+  var css = [];
+
+  css += "<style>.mapjs-loader{ position:relative; width: 6px; height: 6px; -webkit-animation: line 1s linear infinite alternate; -moz-animation: line 1s linear infinite alternate; animation: line 1s linear infinite alternate;}";
+  css += "@-webkit-keyframes line{ 0%{ background-color: rgba(0,0,0, 1); box-shadow: 12px 0px 0px 0px rgba(0,0,0,0.2), 24px 0px 0px 0px rgba(0,0,0,0.2);} 25%{ background-color: rgba(0,0,0, 0.4); box-shadow: 12px 0px 0px 0px rgba(0,0,0,2), 24px 0px 0px 0px rgba(0,0,0,0.2);} 75%{background-color: rgba(0,0,0, 0.4); box-shadow: 12px 0px 0px 0px rgba(0,0,0,0.2), 24px 0px 0px 0px rgba(0,0,0,2);}}";
+  css += "@-moz-keyframes line{ 0%{ background-color: rgba(0,0,0, 1); box-shadow: 12px 0px 0px 0px rgba(0,0,0,0.2), 24px 0px 0px 0px rgba(0,0,0,0.2); } 25%{ background-color: rgba(0,0,0, 0.4); box-shadow: 12px 0px 0px 0px rgba(0,0,0,2), 24px 0px 0px 0px rgba(0,0,0,0.2);} 75%{background-color: rgba(0,0,0, 0.4);box-shadow: 12px 0px 0px 0px rgba(0,0,0,0.2), 24px 0px 0px 0px rgba(0,0,0,2);}}";
+  css += "@keyframes line{0%{background-color: rgba(0,0,0, 1);box-shadow: 12px 0px 0px 0px rgba(0,0,0,0.2),24px 0px 0px 0px rgba(0,0,0,0.2);}25%{background-color: rgba(0,0,0, 0.4);box-shadow: 12px 0px 0px 0px rgba(0,0,0,2),24px 0px 0px 0px rgba(0,0,0,0.2);}75%{ background-color: rgba(0,0,0, 0.4);box-shadow: 12px 0px 0px 0px rgba(0,0,0,0.2),24px 0px 0px 0px rgba(0,0,0,2);}}</style>";
+
+  helper.html("#mapcss", css);
+}
+
+function __startApp() {
     var list = mapProperties.list;
-
 
     for (i = 0; i < list.length; i++) {
 
       if (pointer != null) {
-
+        //do nothing
       } else {
 
           el = '#' + list[i];
@@ -68,13 +83,11 @@ function __startApp () {
 
           switch (set) {
 
-              case "here": map.getMap(el, width, height, zoom);
+              case "map": map.getMap(el, latitude, longitude, width, height, zoom);
                   break;
               case "latitude": map.getLatitude(el);
                   break;
               case "longitude": map.getLongitude(el);
-                  break;
-              case "map": map.setMap(el, latitude, longitude, width, height, zoom);
                   break;
 
               default: map.getMap(width, height, zoom);
@@ -83,40 +96,29 @@ function __startApp () {
 
           if ( pointer != null )
             list.splice(i, 1);
-
       }
-
-          // console.log(list);
     }
-
-    // console.log(pointer);
 
     if (pointer === null)
       clearInterval(__creating);
 }
 
-//Code to get Maps from Google
+
 window.google = window.google || {};
   google.maps = google.maps || {};
   (function() {
-
     function getScript(src) {
-      document.write('<' + 'script src="' + src + '"' +
-                     ' type="text/javascript"><' + '/script>');
-    }
-
+      document.write('<' + 'script src="' + src + '"' + ' type="text/javascript"><' + '/script>');}
     var modules = google.maps.modules = {};
     google.maps.__gjsload__ = function(name, text) {
       modules[name] = text;
     };
-
     google.maps.Load = function(apiLoad) {
       delete google.maps.Load;
       apiLoad([0.009999999776482582,[[["http://mt0.googleapis.com/vt?lyrs=m@231000000\u0026src=api\u0026hl=pt-BR\u0026","http://mt1.googleapis.com/vt?lyrs=m@231000000\u0026src=api\u0026hl=pt-BR\u0026"],null,null,null,null,"m@231000000"],[["http://khm0.googleapis.com/kh?v=137\u0026hl=pt-BR\u0026","http://khm1.googleapis.com/kh?v=137\u0026hl=pt-BR\u0026"],null,null,null,1,"137"],[["http://mt0.googleapis.com/vt?lyrs=h@231000000\u0026src=api\u0026hl=pt-BR\u0026","http://mt1.googleapis.com/vt?lyrs=h@231000000\u0026src=api\u0026hl=pt-BR\u0026"],null,null,null,null,"h@231000000"],[["http://mt0.googleapis.com/vt?lyrs=t@131,r@231000000\u0026src=api\u0026hl=pt-BR\u0026","http://mt1.googleapis.com/vt?lyrs=t@131,r@231000000\u0026src=api\u0026hl=pt-BR\u0026"],null,null,null,null,"t@131,r@231000000"],null,null,[["http://cbk0.googleapis.com/cbk?","http://cbk1.googleapis.com/cbk?"]],[["http://khm0.googleapis.com/kh?v=81\u0026hl=pt-BR\u0026","http://khm1.googleapis.com/kh?v=81\u0026hl=pt-BR\u0026"],null,null,null,null,"81"],[["http://mt0.googleapis.com/mapslt?hl=pt-BR\u0026","http://mt1.googleapis.com/mapslt?hl=pt-BR\u0026"]],[["http://mt0.googleapis.com/mapslt/ft?hl=pt-BR\u0026","http://mt1.googleapis.com/mapslt/ft?hl=pt-BR\u0026"]],[["http://mt0.googleapis.com/vt?hl=pt-BR\u0026","http://mt1.googleapis.com/vt?hl=pt-BR\u0026"]],[["http://mt0.googleapis.com/mapslt/loom?hl=pt-BR\u0026","http://mt1.googleapis.com/mapslt/loom?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt?hl=pt-BR\u0026"]],[["https://mts0.googleapis.com/mapslt/ft?hl=pt-BR\u0026","https://mts1.googleapis.com/mapslt/ft?hl=pt-BR\u0026"]]],["pt-BR","US",null,0,null,null,"http://maps.gstatic.com/mapfiles/","http://csi.gstatic.com","https://maps.googleapis.com","http://maps.googleapis.com"],["http://maps.gstatic.com/intl/pt_br/mapfiles/api-3/14/3","3.14.3"],[2853384449],1,null,null,null,null,1,"",null,null,0,"http://khm.googleapis.com/mz?v=137\u0026",null,"https://earthbuilder.googleapis.com","https://earthbuilder.googleapis.com",null,"http://mt.googleapis.com/vt/icon",[["http://mt0.googleapis.com/vt","http://mt1.googleapis.com/vt"],["https://mts0.googleapis.com/vt","https://mts1.googleapis.com/vt"],[null,[[0,"m",231000000]],[null,"pt-BR","US",null,18,null,null,null,null,null,null,[[47],[37,[["smartmaps"]]]]],0],[null,[[0,"m",231000000]],[null,"pt-BR","US",null,18,null,null,null,null,null,null,[[47],[37,[["smartmaps"]]]]],3],[null,[[0,"h",231000000]],[null,"pt-BR","US",null,18,null,null,null,null,null,null,[[50],[37,[["smartmaps"]]]]],0],[null,[[0,"h",231000000]],[null,"pt-BR","US",null,18,null,null,null,null,null,null,[[50],[37,[["smartmaps"]]]]],3],[null,[[4,"t",131],[0,"r",131000000]],[null,"pt-BR","US",null,18,null,null,null,null,null,null,[[5],[37,[["smartmaps"]]]]],0],[null,[[4,"t",131],[0,"r",131000000]],[null,"pt-BR","US",null,18,null,null,null,null,null,null,[[5],[37,[["smartmaps"]]]]],3],[null,null,[null,"pt-BR","US",null,18],0],[null,null,[null,"pt-BR","US",null,18],3],[null,null,[null,"pt-BR","US",null,18],6],[null,null,[null,"pt-BR","US",null,18],0]]], loadScriptTime);
     };
     var loadScriptTime = (new Date).getTime();
     getScript("http://maps.gstatic.com/intl/pt_br/mapfiles/api-3/14/3/main.js");
-
 })();
 
 
@@ -137,13 +139,12 @@ function Helper() {
     this.getSize = function(value) {
       return value.replace("px", "");
     }
-
 }
+
 
 function View() {
 
   this.returnError = function(msg) {
-
       src = view.getScreen();
 
       if (src) {
@@ -156,14 +157,12 @@ function View() {
       if (msg) {
         console.log(msg);
       }
-
   }
 
   this.getScreen = function(){
       var src = "http://maps.googleapis.com/maps/api/staticmap?z=" + mapProperties.zoom + "&size=" + helper.getSize(mapProperties.sizeW) + "x" + helper.getSize(mapProperties.sizeH) + "&t=m&markers=color:red%7Ccolor:red%7Clabel:C%7C" + mapProperties.latitude + "," + mapProperties.longitude + "&sensor=false";
       return src;
   }
-
 }
 
 
@@ -246,8 +245,7 @@ function setLongitude(position){
 //The Map Class
 function Map(){
 
-      //GET MAP
-      this.getMap = function(element, mapWidth, mapHeight, zoom) {
+      this.getMap = function(element, lat, lon, mapWidth, mapHeight, zoom) {
 
           if (mapWidth)
               mapProperties.sizeW = mapWidth;
@@ -258,10 +256,13 @@ function Map(){
           if (zoom)
               mapProperties.zoom = zoom;
 
+          if (lat && lon) {
+              mapProperties.latitude = lat;
+              mapProperties.longitude = lon;
+          }
 
           helper.html(element, '<section><article><span id="' + element.replace("#", "S") +
-                      '">Please wait...</span></article></section></div>');
-
+                      '"><div class="mapjs-loader"></div></span></article></section></div>');
 
           if (pointer != null)
               return false;
@@ -269,9 +270,7 @@ function Map(){
           pointer = element.replace("#", "S");
 
           navigator.geolocation.getCurrentPosition(draw, view.returnError);
-
       }
-
 
       this.getLatitude = function(element) {
           if (pointer != null)
@@ -282,7 +281,6 @@ function Map(){
           navigator.geolocation.getCurrentPosition(setLatitude, view.returnError);
       }
 
-
       this.getLongitude = function(element) {
           if (pointer != null)
               return false;
@@ -291,34 +289,4 @@ function Map(){
 
           navigator.geolocation.getCurrentPosition(setLongitude, view.returnError);
       }
-
-
-      //CREATE MAP USING PARAMETERS
-      this.setMap = function(element, lat, lon, mapWidth, mapHeight, zoom) {
-
-          if(mapWidth)
-              mapProperties.sizeW = mapWidth;
-
-          if(mapHeight)
-              mapProperties.sizeH = mapHeight;
-
-          if(zoom)
-              mapProperties.zoom = zoom;
-
-          mapProperties.latitude = lat;
-          mapProperties.longitude = lon;
-
-
-          helper.html(element, '<section><article><span id="' + element.replace("#", "S") +
-                      '">Please wait...</span></article></section></div>');
-
-          if (pointer != null)
-              return false;
-
-          pointer = element.replace("#", "S");
-
-          navigator.geolocation.getCurrentPosition(draw, view.returnError);
-
-      }
-
 }
